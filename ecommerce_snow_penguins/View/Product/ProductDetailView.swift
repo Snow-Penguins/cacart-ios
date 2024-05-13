4a7b1e9f576aea8280d8f07b5903f337ba619153
[treasure3210] <treasure3210@gmail.com>
2024-05-12 19:46:39 -0600

    Refactor changes

--- ecommerce_snow_penguins/View/Product/ProductDetailView.swift
+++ ecommerce_snow_penguins/View/Product/ProductDetailView.swift
@@ -13,17 +13,40 @@ struct ProductDetailView: View {
     /// Product we get from its parent view.
     let product: Product
 
+    /// Active index to keep track of the current product variant shown.
+    @State private var activeIndex: Int = 0
+
     // MARK: - View Conformance
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 12) {
-            Image(product.imageName)
-                .resizable()
-                .scaledToFit()
+        VStack(alignment: .leading, spacing: Stylesheet.Spacing.spacing12) {
+            if !product.productItems.isEmpty {
+                GroupBox {
+                    TabView(selection: $activeIndex) {
+                        ForEach(product.productImage?.indices ?? 0 ..< 0, id: \.self) { index in
+                            Image(product.productImage?[index] ?? "")
+                                .resizable()
+                                .scaledToFill()
+                                .tag(index)
+                        }
+                    }
+                    .tabViewStyle(PageTabViewStyle())
+                    .padding(.bottom, 10)
+                    .frame(height: 300)
+                }
+            }
+
             Text(product.name)
                 .font(.title.bold())
-            Text("$\(product.price, specifier: "%.2f")")
-                .font(.headline)
+
+            if !product.productItems.isEmpty, activeIndex < product.productItems.count {
+                Text("$\(product.productItems[activeIndex].price, specifier: "%.2f")")
+                    .font(.headline)
+            } else if !product.productItems.isEmpty {
+                Text("$\(product.productItems.first?.price ?? 0, specifier: "%.2f")")
+                    .font(.headline)
+            }
+
             Text("""
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut magna, scelerisque vitae augue et, cursus placerat lorem.
             In nisi lacus, eleifend tincidunt quam et, consequat semper.
@@ -35,6 +58,15 @@ struct ProductDetailView: View {
 }
 
 #Preview {
-    let product = Product(id: 0, category: "Women", name: "Winter Sweater", price: 34.00, imageName: "stacked_pile_of_clothes")
+    let product = Product(
+        id: 1,
+        categoryId: 2,
+        name: "Regular Fit Sando T-Shirt",
+        description: nil,
+        productImage: ["women_regular_clothes"],
+        createdAt: Date(),
+        category: Category(name: "Men"),
+        productItems: [ProductItem(price: 25.00)]
+    )
     return ProductDetailView(product: product)
 }
